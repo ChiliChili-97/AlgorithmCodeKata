f169ecb9fddc2fb92fc78724849a59feb4e4e039
Jang SeokBin <71073365+ChiliChili-97@users.noreply.github.com>
2024-01-09 10:13:42 +0900

    Update 약수의 합.c

--- 프로그래머스/1/12928. 약수의 합/약수의 합.c
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -5,8 +5,8 @@
 int solution(int n) {
     int answer = 0;
     
-    for(int i = 1; i <= n / 2; i++){ // 14일때 i <= 7
-        if (n % i == 0){ // 1 2 
+    for(int i = 1; i <= n / 2; i++){
+        if (n % i == 0){
             answer += i;
         }
     }
